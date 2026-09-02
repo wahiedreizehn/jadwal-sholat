8ed5946d3e539aea9bfdb2960420f6bd2653f265
wahiedreizehn <wahied.13@gmail.com>
2026-09-02 10:05:03 +0800

    Update admin-config.js

--- admin-config.js
+++ admin-config.js
@@ -2,6 +2,6 @@
 // apps-script/Code.gs), dan password yang sama dengan SECRET_KEY di sana.
 
 const ADMIN_CONFIG = {
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzhZECpmJbpOGV1GNXKvW2ZkrE5k5yOeqXJXxEV62IQzeDmnfwWn1H-z6h4O19VeBozOw/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzA2kzg6IZdTEFl9aYrhRNKFRCIu1CV2xgcCTiSC4AypbXLnFlLcFdZN8zulSqkLDLcOQ/exec",
   SECRET: "Bismillah"
 };
